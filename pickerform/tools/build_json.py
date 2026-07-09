203ae4468a9d4a8cd2da80c7ef4e114348c4ac0a
Emmanuel BLANCHARD <61318856+djibian@users.noreply.github.com>
2026-07-09 09:30:25 +0200

    Implement filtering of departments in build_json.py
    Added functionality to filter departments based on postal codes and display the number of establishments.

--- pickerform/tools/build_json.py
+++ pickerform/tools/build_json.py
@@ -1,1 +1,28 @@
+from config import PARQUET_FILE
 
+import polars as pl
+
+
+print("Ouverture du fichier...")
+
+lazy = pl.scan_parquet(PARQUET_FILE)
+
+print("Filtrage des départements...")
+
+df = (
+    lazy
+    .filter(
+        pl.col("codePostalEtablissement")
+        .cast(pl.Utf8)
+        .str.starts_with(("44", "85"))
+    )
+    .collect()
+)
+
+print()
+
+print(f"Nombre d'établissements : {len(df):,}")
+
+print()
+
+print(df.head())
